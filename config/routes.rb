32ab3fdcfacfdb2e77d40da23555a542509e7f91
Cristin O'Connor <cristinjaneo@gmail.com>
2014-03-27 11:21:31 -0500

    final add toggle functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ TestRailsOnDigitalOcean::Application.routes.draw do
   resources :users do
     get '/recipes', to: 'user_recipes#index'
     resources :recipes, only: [:show]
-    resources :collected_recipes, only: [:create]
+    resources :collected_recipes, only: [:create, :destroy]
     resources :grocerylists, only: [:show, :create, :destroy]
     resources :scheduled_recipes, only: [:index, :destroy]
     post '/scheduled_recipes/:recipe_id', to: 'scheduled_recipes#create'
